204ec39561b4c0778d9490cbf370ba7f73333d9e
leocavalcante <lc@leocavalcante.com>
2018-06-10 19:13:43 -0300

    Refactor Shelf

--- dart/bin/shelf.dart
+++ dart/bin/shelf.dart
@@ -1,13 +1,12 @@
-import 'dart:async';
 import 'dart:io';
 
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
 
-Future main() async {
+main() async {
   final file = new File('mockdata.json');
   final data = await file.readAsBytes();
   final handler = (request) => new Response.ok(data);
 
-  await io.serve(handler, '0.0.0.0', 8080);
+  io.serve(handler, '0.0.0.0', 8080);
 }
